205f69b009e897ca5d7e6110b7948d549b9ecedd
Kkonstant <kkonstantdev@gmail.com>
2020-06-08 17:13:18 +0300

    home page mobile version

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
 	$('.ac-parent').on('click',function() {
 		$(this).parents('.acordeon-box').toggleClass('active');
+	});
+
+	$('.filter__item--parent').on('click',function() {
+		$(this).addClass('active');
+		$('.sidebar').addClass('js--open');
+	});
+
+	$('.sidebar .close-icon').on('click',function() {
+		$('.filter__item--parent').removeClass('active');
+		$('.sidebar').removeClass('js--open');
 	})
 });
