d5f16dc288677cb3481132f8b5bae1ab7cc8bb68
Kkonstant <kkonstantdev@gmail.com>
2020-06-09 01:40:24 +0300

    home page mobile sidebar

--- assets/build/js/main.js
+++ assets/build/js/main.js
@@ -3,13 +3,19 @@ $(document).ready(function() {
 		$(this).parents('.acordeon-box').toggleClass('active');
 	});
 
+	$('.ac-parent--sub').on('click',function() {
+		$(this).parents('.sidebar-list--sub-parent').toggleClass('active');
+	});
+
 	$('.filter__item--parent').on('click',function() {
 		$(this).addClass('active');
 		$('.sidebar').addClass('js--open');
+		$('body').css('overflow','hidden');
 	});
 
 	$('.sidebar .close-icon').on('click',function() {
 		$('.filter__item--parent').removeClass('active');
 		$('.sidebar').removeClass('js--open');
+		$('body').css('overflow','');
 	})
 });
