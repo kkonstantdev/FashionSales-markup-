041aef74f7e86cace81532f156637bf85ef07bc7
Kkonstant <kkonstantdev@gmail.com>
2020-06-13 01:19:07 +0300

    fixed filter mobile

--- assets/build/js/main.js
+++ assets/build/js/main.js
@@ -10,40 +10,13 @@ $(document).ready(function() {
 	$('#filter-parent-mobile').on('click',function() {
 		$(this).addClass('active');
 		$('.sidebar').addClass('js--open');
-		$('body').css('overflow','hidden');
+		$('body').addClass('body-overflow');
 	});
 
 	$('.sidebar--home .close-icon').on('click',function() {
 		$('#filter-parent-mobile').removeClass('active');
 		$('.sidebar').removeClass('js--open');
-		$('body').css('overflow','');
-	});
-
-	// ================= Filter scroll desktop ============
-	$(function(){
-		var parentElemWidth = $(".filter-box").width(),
-			innerElemWidth = $(".filter-box--inner").width(),
-	    	margLeft = 0;
-
-	    $('#filter-parent-desktop').on('click',function() {
-	    	margLeft = margLeft+200;
-			if(parentElemWidth < innerElemWidth) {
-				$('.filter-box--inner').css({'margin-left': '-' + margLeft + 'px'});
-			}
-
-			if ($(window).width() > '992'){
-				if(innerElemWidth-800 < margLeft) {
-					$('.filter-box--inner').css({'margin-left': '48px'});
-					margLeft = 0;
-				}
-			}
-			if ($(window).width() <= '992'){
-				if(innerElemWidth-500 < margLeft) {
-					$('.filter-box--inner').css({'margin-left': '48px'});
-					margLeft = 0;
-				}
-			}
-		});
+		$('body').removeClass('body-overflow');
 	});
 
 });
